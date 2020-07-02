6c9d1cd3c8a02eed5ad641595ce28a1e1dc3e292
Viacheslav Petriaiev <viacheslav.petriaiev@gmail.com>
2020-07-02 13:40:56 +0300

    Function as bean testing

--- src/main/kotlin/com/example/demo/cloud/config/CloudConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.example.demo.cloud.config
-
-import com.example.demo.cloud.channel.CloudOperationsChannels
-import org.springframework.cloud.stream.annotation.EnableBinding
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@EnableBinding(CloudOperationsChannels::class)
-class CloudConfig
--- src/test/kotlin/com/example/demo/cloud/listener/OperationsListenerTest.kt
+++ src/test/kotlin/com/example/demo/cloud/listener/OperationsListenerTest.kt
@@ -1,36 +1,38 @@
 package com.example.demo.cloud.listener
 
-import com.example.demo.cloud.channel.CloudOperationsChannels
 import com.example.demo.domain.Person
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.cloud.stream.test.binder.MessageCollector
+import org.springframework.cloud.stream.binder.test.InputDestination
+import org.springframework.cloud.stream.binder.test.OutputDestination
 import org.springframework.messaging.support.GenericMessage
 import org.springframework.test.context.ActiveProfiles
+
 
 @SpringBootTest
 @ActiveProfiles("test")
 internal class OperationsListenerTest {
 
     @Autowired
-    private lateinit var messageCollector: MessageCollector
+    private lateinit var input: InputDestination
 
     @Autowired
-    private lateinit var cloudOperationsChannels: CloudOperationsChannels
+    private lateinit var output: OutputDestination
 
     @Test
     fun operationsListener() {
 
-        cloudOperationsChannels.input().send(
+        input.send(
                 GenericMessage(
                         ObjectMapper().writeValueAsBytes(
                                 Person(firstName = "testFirstName", lastName = "testLastName")
                         )
-                )
+                ),
+                "operationsInputQueue"
         )
-        assertThat(messageCollector.forChannel(cloudOperationsChannels.output())).isNotEmpty
+        assertThat(output.receive(5000, "operationsOutputQueue")).isNotNull
     }
 }
